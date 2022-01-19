3e6892ad04d5cfaf1070f0d0ff1aff2b8d848f29
dennuguyen <dennuguyen@gmail.com>
2022-01-20 07:26:24 +1100

    Rename test as contains.

--- test/test_contains.cpp
+++ test/test_contains.cpp
@@ -1,85 +1,85 @@
 #include "contains.hpp"
 #include "gtest/gtest.h"
 
-TEST(begins_with_contiguous_subset, true) {
+TEST(begins_with_contiguous_subset, contains) {
     std::vector<int> alpha = {0, 3, 2, 6};
     std::vector<int> test = {0, 3, 2};
     EXPECT_TRUE(xtl::contains(alpha, test));
 }
 
-TEST(ends_with_contiguous_subset, true) {
+TEST(ends_with_contiguous_subset, contains) {
     std::vector<int> alpha = {0, 3, 2, 6};
     std::vector<int> test = {3, 2, 6};
     EXPECT_TRUE(xtl::contains(alpha, test));
 }
 
-TEST(duplicated_value_in_alphabet, true) {
+TEST(duplicated_value_in_alphabet, contains) {
     std::string alpha = "abbbbc";
     std::string word = "bb";
     EXPECT_TRUE(xtl::contains(alpha, word));
 }
 
-TEST(identical_sets, true) {
+TEST(identical_sets, contains) {
     std::vector<int> alpha = {0, 3, 2, 6};
     std::vector<int> test = {0, 3, 2, 6};
     EXPECT_TRUE(xtl::contains(alpha, test));
 }
 
-TEST(empty_value, true) {
+TEST(empty_value, contains) {
     std::vector<char> alpha = {'a', 'b', 'c'};
     std::vector<char> word = {};
     EXPECT_TRUE(xtl::contains(alpha, word));
 }
 
-TEST(noncontiguous_subset, false) {
+TEST(noncontiguous_subset, contains) {
     std::vector<int> alpha = {0, 3, 2, 6};
     std::vector<int> test = {0, 2};
     EXPECT_FALSE(xtl::contains(alpha, test));
 }
 
-TEST(consecutive_but_not_contiguous, false) {
+TEST(consecutive_but_not_contiguous, contains) {
     std::vector<int> alpha = {0, 3, 2, 6};
     std::vector<int> test = {2, 3, 0};
     EXPECT_FALSE(xtl::contains(alpha, test));
 }
 
-TEST(single_char_alphabet, false) {
+TEST(single_char_alphabet, contains) {
     std::string alpha = "a";
     std::string word = "aaaaaaa";
     EXPECT_FALSE(xtl::contains(alpha, word));
 }
 
-TEST(single_value_duplicated, false) {
+TEST(single_value_duplicated, contains) {
     std::string alpha = "abcdefghijklmnopqrstuvwxyz";
     std::string word = "hhhhh";
     EXPECT_FALSE(xtl::contains(alpha, word));
 }
 
-TEST(multiple_values_duplicated, false) {
+TEST(multiple_values_duplicated, contains) {
     std::string alpha = "abcdefghijklmnopqrstuvwxyz";
     std::string word = "abcabcabcabc";
     EXPECT_FALSE(xtl::contains(alpha, word));
 }
 
-TEST(single_missing_value, false) {
+TEST(single_missing_value, contains) {
     std::string alpha = "orms";
     std::string word = "worms";
     EXPECT_FALSE(xtl::contains(alpha, word));
 }
 
-TEST(empty_alphabet, false) {
+TEST(empty_alphabet, contains) {
     std::vector<int> alpha = {};
     std::vector<int> word = {0, 2, 3, 5, 6, 71, 1};
     EXPECT_FALSE(xtl::contains(alpha, word));
 }
 
-TEST(both_empty_sets, false) {
+TEST(both_empty_sets, contains) {
     std::vector<int> alpha = {};
     std::vector<int> test = {};
     EXPECT_FALSE(xtl::contains(alpha, test));
 }
 
-TEST(sentence1, sentences) {
+TEST(sentence1, contains) {
     std::string alpha = "I am writing a contains template function";
     std::string word1 = "I";
     std::string word2 = "am";
