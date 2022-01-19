88d1f03ed662d79bdd68e3a080bd2ba0790d8c15
dennuguyen <dennuguyen@gmail.com>
2022-01-20 07:24:29 +1100

    Add new lines.

--- include/transform_if.hpp
+++ include/transform_if.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 namespace xtl {
+
 // transform_if behaves like std::transform but has a unary_pred which conditionally executes unary_op.
 template <typename InputIt, typename OutputIt, typename UnaryPredicate, typename UnaryOperation>
 auto transform_if(InputIt first1, InputIt last1, OutputIt d_first, UnaryPredicate unary_pred, UnaryOperation unary_op) {
@@ -11,4 +12,5 @@ auto transform_if(InputIt first1, InputIt last1, OutputIt d_first, UnaryPredicat
     }
     return d_first;
 }
+
 }  // namespace xtl
