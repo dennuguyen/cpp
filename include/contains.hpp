2e5f6f8d405d9c7cb2919b5f93eab85bdbfde2e3
dennuguyen <dennuguyen@gmail.com>
2022-01-20 07:15:50 +1100

    Create xtl namespace.

--- include/contains.hpp
+++ include/contains.hpp
@@ -2,14 +2,18 @@
 
 #include <algorithm>
 
+namespace xtl {
+
 // Returns true if the container 1 contains container 2.
 template <class Container1, class Container2>
 bool contains(Container1 cont1, Container2 cont2) {
     return std::search(cont1.begin(), cont1.end(), cont2.begin(), cont2.end()) != cont1.end();
 }
 
-// Returns true if the container 1 contains container 2.
+// Returns true if the container 1 contains container 2 given a binary predicate.
 template <class Container1, class Container2, class BinaryPredicate>
-bool contains(Container1 cont1, Container2 cont2, BinaryPredicate pred) {
-    return std::search(cont1.begin(), cont1.end(), cont2.begin(), cont2.end(), pred) != cont1.end();
+bool contains(Container1 cont1, Container2 cont2, BinaryPredicate binary_pred) {
+    return std::search(cont1.begin(), cont1.end(), cont2.begin(), cont2.end(), binary_pred) != cont1.end();
 }
+
+} // namespace xtl
