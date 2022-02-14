e496f9cf281636ffdde82ff786807a74c4115af6
Dan Nguyen <dennuguyen@gmail.com>
2022-02-14 11:23:25 +0000

    Initial commit.

---
+++ include/binary_search_index.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <algorithm>
+#include <functional>
+
+namespace xtl {
+
+// Returns the index of the target value in a sorted container otherwise the index outside the range.
+template<class ForwardIt, class T, class Compare = std::less<>>
+auto binary_search_value(ForwardIt first, ForwardIt last, const T& value, Compare comp = Compare{}) -> T {
+    return std::distance(first, std::lower_bound(first, last, value, comp));;
+}
+
+}  // namespace xtl
