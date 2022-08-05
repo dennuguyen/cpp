0f66a6997496a753fc451ca9a0bdcb5d2c83d2b1
dennuguyen <dennuguyen@gmail.com>
2022-08-05 14:35:24 +1000

    Fix test name.

--- maximum_disjoint_set/test_maximum_disjoint_set.cpp
+++ maximum_disjoint_set/test_maximum_disjoint_set.cpp
@@ -1,14 +1,14 @@
 #include "gtest/gtest.h"
 #include "maximum_disjoint_set.hpp"
 
-TEST(non_overlapping_string, maximum_disjoint_set) {
+TEST(maximum_disjoint_set, non_overlapping_string) {
     auto str = std::string("abcdefghijk");
     auto input = std::vector<std::pair<std::string::iterator, std::string::iterator>>({{str.begin(), str.begin() + 1}, {str.begin() + 3, str.begin() + 4}});
     auto output = xtd::maximum_disjoint_set(input);
     EXPECT_EQ(2, output.size());
 }
 
-TEST(overlapping_string, maximum_disjoint_set) {
+TEST(maximum_disjoint_set, overlapping_string) {
     auto str = std::string("abcdefghijk");
     auto input = std::vector<std::pair<std::string::iterator, std::string::iterator>>({{str.begin(), str.begin() + 3}, {str.begin() + 2, str.begin() + 4}, {str.begin() + 4, str.begin() + 6}});
     auto output = xtd::maximum_disjoint_set(input);
