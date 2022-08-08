730ef027f987757a8f9a948883fab1d171773e30
dennuguyen <dennuguyen@gmail.com>
2022-08-09 03:41:38 +1000

    Write more tests.

--- invert_element_order/test_invert_element_order.cpp
+++ invert_element_order/test_invert_element_order.cpp
@@ -8,6 +8,20 @@ TEST(invert_element_order, numerical) {
     EXPECT_EQ(w, v);
 }
 
+TEST(invert_element_order, same) {
+    auto v = std::vector<int>({1, 1, 1, 1});
+    xtd::invert_element_order(v.begin(), v.end());
+    auto w = std::vector<int>({1, 1, 1, 1});
+    EXPECT_EQ(w, v);
+}
+
+TEST(invert_element_order, one_out) {
+    auto v = std::vector<int>({1, 1, 1, 2});
+    xtd::invert_element_order(v.begin(), v.end());
+    auto w = std::vector<int>({2, 2, 2, 1});
+    EXPECT_EQ(w, v);
+}
+
 TEST(invert_element_order, alpha) {
     auto v = std::vector<std::string>({"abc", "cba", "bde"});
     xtd::invert_element_order(v.begin(), v.end());
