abafea2e9842843f1eade2b9a1042fc4ee4caae4
dennuguyen <dennuguyen@gmail.com>
2022-08-09 03:18:05 +1000

    Fix seg fault.

--- invert_element_order/invert_element_order.hpp
+++ invert_element_order/invert_element_order.hpp
@@ -9,16 +9,18 @@ namespace xtd {
 
 template <typename Iter, typename T = typename std::iterator_traits<Iter>::value_type>
 auto invert_element_order(Iter first, Iter last) -> void {
-    auto elems_ascending_order = std::vector<T>();
+    auto const size = std::distance(first, last);
+
+    auto elems_ascending_order = std::vector<T>(size);
     std::copy(first, last, elems_ascending_order.begin());
     std::sort(elems_ascending_order.begin(), elems_ascending_order.end());
 
-    auto elems_descending_order = std::vector<T>();
+    auto elems_descending_order = std::vector<T>(size);
     std::copy(first, last, elems_descending_order.begin());
     std::sort(elems_descending_order.rbegin(), elems_descending_order.rend());
 
     auto inverted_map = std::map<T, T>();
-    for (std::size_t i = 0; i < std::distance(first, last); i++) {
+    for (std::size_t i = 0; i < size; i++) {
         inverted_map[elems_ascending_order[i]] = elems_descending_order[i];
     }
 
