4801ea7c23a9ee693308da4f71c56e171a867bd5
dennuguyen <dennuguyen@gmail.com>
2024-11-07 14:32:56 +1100

    Add size and capacity.

--- circular_queue/circular_queue.hpp
+++ circular_queue/circular_queue.hpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <cstddef>
 
-template <typename T, std::size_t capacity>
+template <typename T, std::size_t capacity_>
 class CircularQueue {
 public:
     template <typename IteratorType>
@@ -75,7 +75,7 @@ public:
 
     CircularQueue() : CircularQueue({}) {}
 
-    CircularQueue(std::initializer_list<T> il) : size(0), head(data), tail(data) {
+    CircularQueue(std::initializer_list<T> il) : size_(0), head(data), tail(data) {
         for (auto const& e : il) push(e);
     }
 
@@ -91,8 +91,10 @@ public:
     // auto cbegin() const -> const const_iterator_type { return {head, tail, tail}; }
     // auto cend() const -> const const_iterator_type { return {head, tail, head}; }
 
-    auto full() const -> bool { return size == capacity; }
-    auto empty() const -> bool { return size == 0; }
+    auto full() const -> bool { return size_ == capacity_; }
+    auto empty() const -> bool { return size_ == 0; }
+    auto size() const -> size_t { return size_; }
+    auto capacity() const -> size_t { return capacity_; }
 
     auto front() -> T& { return *tail; }
     auto front() const -> T { return *tail; }
@@ -101,37 +103,37 @@ public:
         if (full()) pop();
         *head = item;
         head = wrap(head + 1);
-        if (size < capacity) size++;
+        if (size_ < capacity_) size_++;
         return true;
     }
 
     auto pop() -> bool {
         if (empty()) return false;
         tail = wrap(tail + 1);
-        size--;
+        size_--;
         return true;
     }
 
     friend std::ostream& operator<<(std::ostream& os, CircularQueue const& cq) {
-        for (std::size_t i = 0; i < cq.size; ++i) {
-            os << cq[i] << (i < cq.size - 1 ? ", " : "");
+        for (std::size_t i = 0; i < cq.size_; ++i) {
+            os << cq[i] << (i < cq.size_ - 1 ? ", " : "");
         }
         return os;
     }
 
 private:
     auto wrap(T* ptr) -> T* {
-        T* end = data + capacity;
+        T* end = data + capacity_;
         return ptr >= end ? data + (ptr - end) : ptr;
     }
 
     auto wrap(T* ptr) const -> const T* {
-        const T* end = data + capacity;
+        const T* end = data + capacity_;
         return ptr >= end ? data + (ptr - end) : ptr;
     }
 
-    size_t size;       // Required to handle size 0 and 1 cases.
-    T data[capacity];  // Data is stored in reverse [tail, head].
-    T* head;           // One-after-the-element that was pushed.
-    T* tail;           // Element to be popped first.
+    size_t size_;       // Required to handle size 0 and 1 cases.
+    T data[capacity_];  // Data is stored in reverse [tail, head].
+    T* head;            // One-after-the-element that was pushed.
+    T* tail;            // Element to be popped first.
 };
